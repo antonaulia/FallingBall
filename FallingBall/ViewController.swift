b1e5f9ce485b244910c709f391608c84a914520c
antonaulia <antoniusferiantoaulia@gmail.com>
2020-06-10 16:01:23 +0700

    ca animation low level done

--- FallingBall/ViewController.swift
+++ FallingBall/ViewController.swift
@@ -33,8 +33,47 @@ class ViewController: UIViewController {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let coordinateTouch = touches.first?.location(in: view)
-       
-        createBall(at: coordinateTouch)
+        guard let coordinate = coordinateTouch else {return}
+//        membatasi area pembuatan bola
+        let widthView = view.frame.size.width
+        if coordinate.x < (widthView/2) {
+//            createBall(at: coordinate)
+            createSquare(at: coordinate)
+        }
+    }
+    
+    func createSquare(at coordinate:CGPoint?) {
+        let square = UIView()
+        
+        guard let coordinate = coordinate else {return}
+        let size = CGSize(width: 50, height: 50)
+        
+        square.frame = CGRect(origin: coordinate, size: size)
+        square.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        view.addSubview(square)
+        
+        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale.x")
+        scaleAnimation.fromValue = 0
+        scaleAnimation.toValue = 1
+        
+        let cornerRadiusAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.cornerRadius))
+        cornerRadiusAnimation.fromValue = 0
+        cornerRadiusAnimation.toValue = 10
+        
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(3)
+        CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear))
+        
+        square.layer.add(scaleAnimation, forKey: "scaling_animation")
+        square.layer.add(cornerRadiusAnimation, forKey: "corner_radius_animation")
+        
+        CATransaction.setCompletionBlock({
+            square.frame.size = CGSize(width: size.width*2, height: size.height)
+            square.layer.cornerRadius = 10
+            square.layer.masksToBounds = true
+        })
+        CATransaction.commit()
+        
     }
     
     func createBall(at coordinate:CGPoint?){
@@ -54,10 +93,25 @@ class ViewController: UIViewController {
         ball.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         ball.layer.borderWidth = 2
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(BallTapped(_:)))
+        ball.addGestureRecognizer(tap)
+        
         view.addSubview(ball)
         
         gravityBehavior.addItem(ball)
         collisionBehavior.addItem(ball)
+        
+    }
+    
+    @objc func BallTapped(_ sender: UITapGestureRecognizer){
+        let ball = sender.view
+        UIView.animate(withDuration: 1, animations: {
+            ball?.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+            ball?.alpha = 0
+        }) { (isAnimated) in
+            ball?.removeFromSuperview()
+            self.collisionBehavior.removeItem(ball!)
+        }
         
     }
 
